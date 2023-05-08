041df567e3439ec3f060337cf3b1c157b1c60e8b
Taufeeq1996 <100328416+Taufeeq1996@users.noreply.github.com>
2023-05-08 06:39:43 +0000

    learning scope

--- basics/javascript-scope.js
+++ basics/javascript-scope.js
@@ -1,1 +1,37 @@
-console.log('scope');
+// scope
+
+// function scope
+
+// Priyanka Chopra   she is an Indian actress, but she works in Hollywood also, so she is a global star
+// Nick jones     he is husband of Priyanka
+
+// Alia Bhatt   she is also an India actress, but she is not a global star or you can say that she is a local star
+
+let name = 'Priyanka' // name variable is defined inside global scope 
+// name variable is like Priyanka
+
+console.log(name); // global scope
+
+function India(x,y){
+    // z is a local variable
+    console.log(name);    // local scope
+    let Alia = x + y;   // the scope of this z variable is this function is only
+    return Alia;
+}
+
+console.log(India(9,3));
+
+// console.log(Alia); // trying to get the value of z outside the function
+
+// block scope --> if-else, for loop, while scope
+
+{
+    var actress = 'Kareena' // block scope and this variable is local variable
+    // console.log(actress)
+}
+
+console.log(actress);
+
+// let, const ,var
+
+// block scope doesn't work on var variables
