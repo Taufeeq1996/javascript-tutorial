6ca429f92c2e16f4be19632de138f727b6b5ba96
Taufeeq1996 <100328416+Taufeeq1996@users.noreply.github.com>
2023-05-09 02:23:09 +0000

    created content of functions and ternary operator

---
+++ basics/javascript-function.js
@@ -0,0 +1,20 @@
+// javascript funcitons
+
+// functions are basically a piece of code that is used to perform some particular task
+
+// function threeNumMultiply(x,y,z){
+//     return x * y* z;
+// }
+
+// console.log(threeNumMultiply(4,5,6));
+
+// return is the last statement in a function
+
+// function expression
+// arrow function --> it is a new feature in javascript 
+
+const threeNumMultiply = (x,y,z) => {
+    return x * y* z
+}
+
+console.log(threeNumMultiply(4,5,6));
---
+++ basics/javascript-ternary.js
@@ -0,0 +1,21 @@
+// ternary operator --> also a conditional coding
+
+// it is an alternate of if-else 
+
+// if-else
+
+let age = 20;
+
+// if(age > 16){
+//     console.log('You can drive');
+// }else{
+//     console.log('Sorry, you have to wait');
+// }
+
+// ternary 
+
+// condition ? expression(if condition is true) : expression(else condition is false)
+
+age > 16 ? console.log('You can drive') : console.log('Sorry, you have to wait')
+
+// node js is called runtime environment of javascript
